058a6204213b22ceeb61668d0282d4560c53c8fa
Gabriela Stoyanova <gabstoyanova1@gmail.com>
2022-12-26 18:14:56 +0200

    initial commit

---
+++ src/main/java/com/example/bpmfinder/BpmFinderApplication.java
@@ -0,0 +1,13 @@
+package com.example.bpmfinder;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BpmFinderApplication {
+
+  public static void main(String[] args) {
+    SpringApplication.run(BpmFinderApplication.class, args);
+  }
+
+}
---
+++ src/test/java/com/example/bpmfinder/BpmFinderApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.bpmfinder;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BpmFinderApplicationTests {
+
+  @Test
+  void contextLoads() {
+  }
+
+}
